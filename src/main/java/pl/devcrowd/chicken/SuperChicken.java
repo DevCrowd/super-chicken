1500784760d797a0a40f683d9d6d3344bf71193e
spacanowski <spacanowski@gmail.com>
2015-11-08 17:28:18 +0100

    Initial commit

---
+++ src/main/java/pl/devcrowd/chicken/SuperChicken.java
@@ -0,0 +1,13 @@
+package pl.devcrowd.chicken;
+
+import io.dropwizard.Application;
+import io.dropwizard.Configuration;
+import io.dropwizard.setup.Environment;
+
+public class SuperChicken extends Application<Configuration> {
+
+	@Override
+	public void run(Configuration configuration, Environment environment) throws Exception {
+
+	}
+}
