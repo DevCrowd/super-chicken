cd38dc6e9ea239633f70978cca9811ac680b380f
spacanowski <spacanowski@gmail.com>
2015-12-06 20:08:37 +0100

    Added getting all speakers

---
+++ src/main/java/pl/devcrowd/chicken/resource/SpeakerResource.java
@@ -0,0 +1,24 @@
+package pl.devcrowd.chicken.resource;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import pl.devcrowd.chicken.service.SpeakerService;
+
+@Path("/speakers")
+@Produces(MediaType.APPLICATION_JSON)
+public class SpeakerResource {
+	@Inject
+	private SpeakerService service;
+
+	@GET
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response getSpeakers() {
+		return Response.ok().entity(service.getSpeakers()).build();
+	}
+}
