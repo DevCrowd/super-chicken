e895dabfc2fddb99ecba28551edb2575b377234b
spacanowski <spacanowski@gmail.com>
2016-02-29 21:31:45 +0100

    Added forgotten mapper

---
+++ src/main/java/pl/devcrowd/chicken/dao/SpeakerWithoutPictureMapper.java
@@ -0,0 +1,23 @@
+package pl.devcrowd.chicken.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.skife.jdbi.v2.StatementContext;
+import org.skife.jdbi.v2.tweak.ResultSetMapper;
+
+import pl.devcrowd.chicken.model.Speaker;
+
+public class SpeakerWithoutPictureMapper implements ResultSetMapper<Speaker> {
+    @Override
+    public Speaker map(int index, ResultSet r, StatementContext ctx) throws SQLException {
+        Speaker result = new Speaker();
+
+        result.setId(r.getString("id"));
+        result.setFirstname(r.getString("name"));
+        result.setLastname(r.getString("surname"));
+        result.setDescription(r.getString("description"));
+
+        return result;
+    }
+}
